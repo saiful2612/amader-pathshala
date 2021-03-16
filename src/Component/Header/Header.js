83b9de3026b3f101153d9ec7d56bb2b9856ddcd6
saiful2612 <bristykodom@gmail.com>
2021-03-16 19:45:03 +0600

    first part completed

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 import './Header.css';
 
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header portion</h2>
+        <div className="header">
+            <img src={logo} alt=""/>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
